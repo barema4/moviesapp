075fd8e1a8bf20c8643d2aef92ac672dc589f656
sam rubarema <samrubarema6@gmail.com>
2023-03-23 16:26:20 +0300

    add tailwind and style components

--- src/components/MovieDetail.tsx
+++ src/components/MovieDetail.tsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, memo } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
+import "../styles/MovieList.css"
 
 type Movie = {
   imdbID: string;
@@ -9,7 +10,7 @@ type Movie = {
   Poster: string;
   Plot: string;
   Released: string;
-  Runtime: string
+  Runtime: string;
 };
 
 type Params = {
@@ -19,6 +20,7 @@ type Params = {
 const MovieDetail = () => {
   const [movie, setMovie] = useState<Movie | null>(null);
   const { id } = useParams<Params>();
+
   useEffect(() => {
     axios.get(`https://www.omdbapi.com/?i=${id}&apiKey=6f9dc29f`)
       .then(response => {
@@ -30,19 +32,59 @@ const MovieDetail = () => {
   }, [id]);
 
   if (!movie) {
-    return <div>Loading...</div>;
+    return <div className='justify-center'>Loading...</div>;
   }
 
   return (
-    <div>
-      <h1>Title: {movie.Title}</h1>
-      <img src={movie.Poster} alt={movie.Title} />
-      <p>Year: {movie.Year}</p>
-      <p>Plot: {movie.Plot}</p>
-      <p>Released: {movie.Released}</p>
-      <p>Runtime: {movie.Runtime}</p>
+    <div className='m-8 flex flex-wrap flex-row justify-center movie-details items-center'>
+      <div className='basis-1/2 md:shrink-0'>
+        <img src={movie.Poster} alt={movie.Title} className="w-[500px] h-[350px]" />
+      </div>
+      <div className='basis-1/3'>
+        <div className='grid grid-cols-2 gap-3 mb-3'>
+          <div className='text-xl subpixel-antialiased font-medium'>
+            Title
+          </div>
+          <div className='text-sm subpixel-antialiased font-medium'>
+            {movie.Title}
+          </div>
+        </div>
+        <div className='grid grid-cols-2 gap-3 mb-3'>
+          <div className='text-xl subpixel-antialiased font-medium'>
+            Year
+          </div>
+          <div className='text-normal subpixel-antialiased font-medium'>
+            {movie.Year}
+          </div>
+        </div>
+        <div className='grid grid-cols-2 gap-3 mb-3'>
+          <div className='text-xl subpixel-antialiased font-medium'>
+            Plot
+          </div>
+          <div className='text-normal'>
+            {movie.Plot}
+          </div>
+        </div>
+        <div className='grid grid-cols-2 gap-3 mb-3'>
+          <div className='text-xl subpixel-antialiased font-medium'>
+            Released
+          </div>
+          <div className='text-normal'>
+            {movie.Released}
+          </div>
+        </div>
+        <div className='grid grid-cols-2 gap-3 mb-3'>
+          <div className='text-xl subpixel-antialiased font-medium'>
+            Runtime
+          </div>
+          <div className='text-normal'>
+            {movie.Runtime}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default MovieDetail;
+export default memo(MovieDetail);
+
--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -1,6 +1,7 @@
 import React,{ useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import "../styles/MovieList.css"
 
 type Movie = {
   imdbID: string;
@@ -12,55 +13,80 @@ type Movie = {
 const MovieList = () => {
   const [movies, setMovies] = useState<Movie[]>([]);
   const [searchTerm, setSearchTerm] = useState('');
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    console.log('movies', movies);
+  }, [movies])
   
-  const MOVIE_API_URL = "https://www.omdbapi.com/?s=man&apikey=6f9dc29f";
+  
+  const MOVIE_API_URL = `https://www.omdbapi.com/?s=man&page=${page}&apikey=6f9dc29f`;
   useEffect(() => {
     axios.get(MOVIE_API_URL)
     .then(response => {
-      setMovies(response.data.Search);
+      setMovies(prevMovies => [ ...prevMovies, ...response.data.Search,]);
     })
     .catch(error => {
       console.log(error);
     });
-  }, []);
+  }, [page]);
 
   const handleSearch = (event: React.FormEvent<HTMLFormElement>) => {
-    
+    setMovies([])
     event.preventDefault();
 
     axios.get(`https://www.omdbapi.com/?s=${searchTerm}&apikey=6f9dc29f`)
     .then(response => {
+      console.log(response.data.Search, '++++')
       setMovies(response.data.Search);
-      setSearchTerm('')
     })
     .catch(error => {
       console.log(error);
     });
   };
+
+
+  const handleScroll = () => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop ===
+      document.documentElement.offsetHeight
+    ) {
+      setPage(prevPage => prevPage + 1);
+    }
+  };
+
+
   let poster
   const DEFAULT_PLACEHOLDER_IMAGE =
   "https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg";
-
+  
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
-    <div>
-      <h1>Popular Movies</h1>
-      <form onSubmit={handleSearch}>
-        <input type="text" value={searchTerm} onChange={event => setSearchTerm(event.target.value)} />
-        <button type="submit">Search</button>
-      </form>
-      <ul>
-        {movies.map(movie => {
-           
+    <div className="m-8">
+      <div className='flex flex-col items-center'>
+          <h1 className="ml-6 text-6xl font-bold font-serif text-slate-300">The Movie Search</h1>
+          <form className="m-6 flex flex-col items-center" onSubmit={handleSearch}>
+            <input className='text-[#e2e8f0] px-4 py-2 w-96 rounded-lg bg-[#334155] outline-0 ring-4 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500' type="text" value={searchTerm} onChange={event => setSearchTerm(event.target.value)} placeholder="Spider..." />
+            <button className="bg-[#0ea5e9] rounded-lg text-white mt-4 w-48 px-2 py-2 border-0 bg-sky-500 hover:bg-sky-700" type="submit">Search</button>
+          </form>
+      </div>
+      
+      <ul className="flex flex-wrap gap-4 justify-center">
+        {movies?.map((movie, i) => {
+           console.log('movie', movie, i);
            poster = movie.Poster === "N/A" ? DEFAULT_PLACEHOLDER_IMAGE : movie.Poster;
             return (
                 
                 <li key={movie.imdbID}>
-                  <Link to={`/${movie.imdbID}`}>
-                    <img src={poster} alt={movie.Title} />
-                    <div>
-                      <h2>{movie.Title}</h2>
-                      <p>Released: {movie.Year}</p>
+                  <Link to={`/${movie.imdbID}`} className='movie-item'>
+                    <img src={poster} alt={movie.Title} className="w-[250px] h-[350px]"/>
+                    <div className="flex flex-col items-center">
+                      <p className='text-[14px] w-[150px] mt-4 font-bold text-center'>Title :{movie.Title}</p>
+                      <p className='text-[14px] w-[150px] mt-4 font-bold text-center'>Released: {movie.Year}</p>
                     </div>
                   </Link>
                 </li>
